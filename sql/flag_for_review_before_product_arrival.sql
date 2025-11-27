90e0a63a591b7e24689868b3818ad36a816c4abe
Jesus Castillo <67293942+oscillatingneutrino@users.noreply.github.com>
2025-11-27 03:32:16 -0800

    Update flag_for_review_before_product_arrival.sql

--- sql/flag_for_review_before_product_arrival.sql
+++ sql/flag_for_review_before_product_arrival.sql
@@ -1,4 +1,18 @@
-
+WITH cte AS ( 
+SELECT
+	o.user_id,
+		SUM( CASE WHEN o.date_number > r.review_date_number THEN 1 ELSE 0 END) AS number_review_before_arrival
+	FROM orders o
+	JOIN reviews r
+		ON o.user_id = r.user_id
+WHERE o.order_status NOT IN('completed','returned')
+GROUP BY o.user_id
+)
+SELECT
+	user_id,
+	CASE WHEN number_review_before_arrival > 0 
+		THEN 'Flag' ELSE 'Normal' END AS review_before_arrival
+	FROM cte;
 
 
 
