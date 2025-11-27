da8109fc2a0e72c2f0f39be1f39c07b5edff9062
Jesus Castillo <67293942+oscillatingneutrino@users.noreply.github.com>
2025-11-27 13:07:21 -0800

    Update attempt.sql

--- sql/attempt.sql
+++ sql/attempt.sql
@@ -1,18 +1,23 @@
 WITH cte AS ( 
 SELECT 
 	product_id, 
-	CASE WHEN 
-		SUM( CASE WHEN event_type = 'purchase' THEN 1 ELSE -1 END) >= 0 
-			THEN 'purchase' ELSE 'wishlist' END AS purchase_or_wishlist 
-	FROM events WHERE event_type IN('purchase','wishlist') 
+	COUNT(CASE WHEN event_type = 'purchase' THEN 1 END) AS purchase_count,
+	COUNT(CASE WHEN event_type = 'wishlist' THEN 1 END) AS wishlist_ccount
+	FROM events 
+	WHERE event_type IN('purchase','wishlist') 
     GROUP BY product_id 
 )
-SELECT 
-p.product_name,
-p.category,
-p.price,
-cte.purchase_or_wishlist
-FROM products p 
-	JOIN cte 
-		ON p.product_id = cte.product_id
-ORDER BY p.price DESC
+SELECT
+	p.product_name,
+	p.product_id,
+	p.category,
+	p.price,
+	CASE WHEN cte.purchase_count * 0.75 > cte.wishlist_count
+		THEN 'purchase'
+	CASE WHEN cte.purchase_count < cte.wishlist_count * 0.75
+		THEN 'wishlist'
+		ELSE 'Marginal Difference'
+		END AS purchase_or_wishlist
+	FROM products p
+		JOIN cte 
+			ON p.product_id = cte.product_id
