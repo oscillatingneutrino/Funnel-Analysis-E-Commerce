9a50a8eaa70576436fc4c6237fc6f74643fee75a
Jesus Castillo <67293942+oscillatingneutrino@users.noreply.github.com>
2025-11-26 17:06:26 -0800

    Create product_count_vs_rating.sql

---
+++ sql/product_count_vs_rating.sql
@@ -0,0 +1,16 @@
+WITH cte AS (
+SELECT
+	oi.product_id,
+    COUNT(oi.product_id) AS product_count
+    FROM order_items oi
+	GROUP BY oi.product_id
+)
+SELECT
+    p.product_name,
+    cte.product_id,
+    cte.product_count,
+    p.rating
+    FROM cte
+	JOIN products p
+		ON cte.product_id = p.product_id
+ORDER BY cte.product_count DESC
