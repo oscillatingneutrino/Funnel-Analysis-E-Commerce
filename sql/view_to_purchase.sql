b6b4c703d3c7f02f37fc65c465728c6182838c20
Jesus Castillo <67293942+oscillatingneutrino@users.noreply.github.com>
2025-11-28 03:49:40 -0800

    Create view_to_purchase.sql

---
+++ sql/view_to_purchase.sql
@@ -0,0 +1,26 @@
+WITH cte AS(
+	SELECT
+		user_id,
+        product_id,
+		MIN(event_timestamp_number) AS purchase_time
+		FROM events
+        WHERE event_type = 'purchase'
+        GROUP BY user_id,product_id
+),
+cte2 AS ( 
+	SELECT
+	user_id,
+    product_id,
+    MIN(event_timestamp_number) AS view_time
+    FROM events
+    WHERE event_type = 'view'
+	GROUP BY user_id,product_id
+)
+SELECT
+    AVG(cte.purchase_time - cte2.view_time) AS view_to_purchase
+    FROM cte
+		JOIN cte2
+			ON cte.user_id = cte2.user_id
+            AND cte.product_id = cte2.product_id
+
+-- In this dataset, the answer was: -140.76855571428626
