dc1144bf7bf7de74800e40708be501be3f83da0b
Jesus Castillo <67293942+oscillatingneutrino@users.noreply.github.com>
2025-11-28 01:48:37 -0800

    Create signup_time_and_event.sql

---
+++ sql/signup_time_and_event.sql
@@ -0,0 +1,17 @@
+
+-- answers the question of, how long did it take from action to signup or vise versa
+WITH cte AS (
+	SELECT
+		user_id,
+        MIN(signup_date_number) AS earliest_event
+        FROM users
+        GROUP BY user_id
+)
+SELECT
+	AVG(CASE WHEN o.date_number > cte.earliest_event THEN o.date_number - cte.earliest_event END) AS signup_before,
+    ABS(AVG(CASE WHEN o.date_number < cte.earliest_event THEN o.date_number - cte.earliest_event END)) AS signup_after
+        FROM cte
+	JOIN orders o
+		ON cte.user_id = o.user_id
+
+-- Answers for this dataset: signup before: 229.8729409870905, signup after: 229.8729409870905
