8e25ac02e01c969ccdc482cffcb8528de6faccbe
Jesus Castillo <67293942+oscillatingneutrino@users.noreply.github.com>
2025-11-28 00:14:24 -0800

    Create users_with_no_purchases.sql

---
+++ sql/users_with_no_purchases.sql
@@ -0,0 +1,16 @@
+SELECT user_id FROM users
+EXCEPT
+SELECT user_id FROM orders
+ORDER BY user_id ASC;
+
+/*
+-- Answer question on how many users have no purchases
+SELECT
+	COUNT(*)
+FROM (SELECT user_id FROM users
+EXCEPT
+SELECT user_id FROM orders
+ORDER BY user_id ASC) AS t1;
+
+-- In this dataset: 1365
+*/
