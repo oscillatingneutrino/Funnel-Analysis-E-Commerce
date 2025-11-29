b39ce7994fe63f950da2126a67e507efc7fe4a0f
Jesus Castillo <67293942+oscillatingneutrino@users.noreply.github.com>
2025-11-28 23:28:11 -0800

    Create no_review_ratio.sql

---
+++ sql/no_review_ratio.sql
@@ -0,0 +1,23 @@
+WITH cte AS (
+SELECT order_id,product_id
+FROM order_items
+EXCEPT ALL
+SELECT order_id,product_id
+FROM reviews
+),
+cte2 AS (
+SELECT
+COUNT(*) AS counter
+FROM cte
+),
+cte3 AS (
+	SELECT
+	COUNT(*) AS other_counter
+    FROM reviews
+)
+SELECT
+	counter/ CAST( (cte3.other_counter + counter) AS FLOAT ) no_review_ratio
+    FROM cte2
+		JOIN cte3
+
+-- For this dataset: 0.6553546400753625
