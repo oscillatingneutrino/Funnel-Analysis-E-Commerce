20d32d981a865b3d027c588226f348e76b174f9d
Jesus Castillo <67293942+oscillatingneutrino@users.noreply.github.com>
2025-11-28 21:20:08 -0800

    Create completed_vs_returned_per_product.sql

---
+++ completed_vs_returned_per_product.sql
@@ -0,0 +1,16 @@
+WITH cte AS (
+SELECT
+	oi.product_id,
+	COUNT(CASE WHEN o.order_status = 'completed' THEN 1 END) AS completed_orders,
+	COUNT(CASE WHEN o.order_status = 'returned' THEN 1 END) AS returned_orders
+	FROM orders o
+		JOIN order_items oi
+			ON o.order_id = oi.order_id
+	GROUP BY oi.product_id
+)
+SELECT
+	p.product_name,
+	ROUND(completed_orders/CAST( (completed_orders + returned_orders) AS FLOAT), 2) AS completed_ratio
+	FROM cte
+		JOIN products p
+			ON cte.product_id = p.product_id;
