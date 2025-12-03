157ae7f7a992c0b095e73ca03ca172cae0f42803
Jesus Castillo <67293942+oscillatingneutrino@users.noreply.github.com>
2025-12-03 00:51:51 -0800

    Update purchase_times.sql

--- sql/purchase_times.sql
+++ sql/purchase_times.sql
@@ -1,5 +1,5 @@
 -- This code calculates how many times a user has used the services to purchase an item
-
+WITH cte AS(
 SELECT
 	oi.user_id,
 	COUNT(DISTINCT oi.order_id) AS purchase_times
@@ -8,5 +8,13 @@ SELECT
 		ON oi.order_id = o.order_id
 WHERE order_status IN ('completed','processing','shipped')
 GROUP BY oi.user_id
-HAVING COUNT(DISTINCT oi.order_id) > 1
-ORDER BY COUNT(oi.order_id) DESC
+)
+SELECT
+	cte.user_id,
+    u.name,
+    purchase_times,
+    DENSE_RANK() OVER (ORDER BY purchase_times DESC) as ranking
+    FROM cte
+    JOIN users u
+		 ON cte.user_id = u.user_id
+ORDER BY purchase_times DESC, u.name ASC
