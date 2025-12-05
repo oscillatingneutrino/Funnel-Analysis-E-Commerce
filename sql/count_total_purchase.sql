361576c8915d81238d06759bae7c3243e6207a70
Jesus Castillo <67293942+oscillatingneutrino@users.noreply.github.com>
2025-12-04 18:37:44 -0800

    Create count_total_purchase.sql

---
+++ sql/count_total_purchase.sql
@@ -0,0 +1,16 @@
+-- This product counts items bought from Harbor or GreenLeaf
+
+SELECT
+	brand,
+    COUNT(event_id) AS count_total_purchase
+    FROM products p
+    JOIN events e
+		ON p.product_id = e.product_id
+	JOIN orders o
+		on e.user_id = o.user_id
+	WHERE event_type = 'purchase'
+    AND category = 'clothing'
+    AND (brand = 'Harbor' OR brand = 'GreenLeaf')
+    AND order_status NOT IN ('returned','cancelled')
+    GROUP BY brand
+    ORDER BY COUNT(event_id) DESC
