f788b27a5c4a38cc4228025e95c1cb4916e3b37f
Jesus Castillo <67293942+oscillatingneutrino@users.noreply.github.com>
2025-12-05 15:55:21 -0800

    Update count_total_purchase_distinct.sql

--- sql/AB_TESTING/count_total_purchase_distinct.sql
+++ sql/AB_TESTING/count_total_purchase_distinct.sql
@@ -1,27 +1,17 @@
 -- This product counts items bought from Harbor or GreenLeaf from distinct users
 
-WITH cte AS(
 SELECT
 	brand,
-    o.user_id,
-    COUNT(event_id) AS counter
-    FROM products p
-    JOIN events e
-		ON p.product_id = e.product_id
+    COUNT(DISTINCT oi.user_id) AS count_total_purchase
+    FROM order_items oi
+    JOIN products p
+		ON p.product_id = oi.product_id
 	JOIN orders o
-		on e.user_id = o.user_id
-	WHERE event_type = 'purchase'
-    AND category = 'clothing'
+		on oi.order_id = o.order_id
+    WHERE category = 'clothing'
     AND (brand = 'Harbor' OR brand = 'GreenLeaf')
     AND order_status NOT IN ('returned','cancelled')
-    GROUP BY brand, user_id
-)
-SELECT
-	brand,
-    COUNT(counter) AS count_total_purchase_distinct
-    FROM cte
-    WHERE counter = 1
     GROUP BY brand
-    ORDER BY COUNT(counter) DESC
+    ORDER BY count_total_purchase DESC
 
--- Answer for this dataset: 16 GreenLeaf,13 Harbor
+-- Answer for this dataset: 346 Harbor, 337 GreenLeaf
