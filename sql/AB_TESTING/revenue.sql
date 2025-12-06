3e974910238533bfc0ce4014e91f50a65421e3f3
Jesus Castillo <67293942+oscillatingneutrino@users.noreply.github.com>
2025-12-05 16:13:49 -0800

    Create revenue.sql

---
+++ sql/AB_TESTING/revenue.sql
@@ -0,0 +1,18 @@
+-- This code gets the total revenue for the two companies mentioned
+-- for their clothinf line
+
+SELECT
+	brand,
+    ROUND(SUM(item_total),2) AS revenue
+    FROM order_items oi
+    JOIN products p
+		ON p.product_id = oi.product_id
+	JOIN orders o
+		on oi.order_id = o.order_id
+    WHERE category = 'clothing'
+    AND (brand = 'Harbor' OR brand = 'GreenLeaf')
+    AND order_status NOT IN ('returned','cancelled')
+    GROUP BY brand
+    ORDER BY revenue DESC
+
+-- Answer: GreenLeaf: 57,842.83, Harbor: 45,550.82
