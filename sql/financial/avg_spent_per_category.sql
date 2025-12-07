fc4fa3ae7d17803b18ed7f6942c4d2e47b0d4a23
Jesus Castillo <67293942+oscillatingneutrino@users.noreply.github.com>
2025-12-06 19:11:15 -0800

    Update avg_spent_per_category.sql

--- sql/financial/avg_spent_per_category.sql
+++ sql/financial/avg_spent_per_category.sql
@@ -1,9 +1,12 @@
--- This code calculates the average amount spent per order per brand per category
+-- This code calculates the average amount spent per order per brand per category per item
+-- averages are done in tableau, but this method allows for product comparison
+-- if need be
 
 WITH cte AS (
 SELECT
 	p.brand,
 	p.category,
+    p.product_name,
 	oi.order_id,
 	SUM(oi.item_total) AS sum_spent_per_category
     FROM order_items oi
@@ -12,11 +15,13 @@ SELECT
 	JOIN orders o
 		ON oi.order_id = o.order_id
 	WHERE order_status NOT IN ('returned','cancelled')
-	GROUP BY oi.order_id,p.brand,p.category
+	GROUP BY oi.order_id,p.brand,p.category,p.product_name
 )
 SELECT
 	brand,
 	cte.category,
+    product_name,
 	ROUND(AVG(sum_spent_per_category),2) AS avg_spent_per_category
     FROM cte
-	GROUP BY brand,category
+	GROUP BY brand,category,product_name
+    ORDER BY brand ASC
