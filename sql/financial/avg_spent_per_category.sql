768795693326419b0ff33218dd31a332af61ca98
Jesus Castillo <67293942+oscillatingneutrino@users.noreply.github.com>
2025-12-06 19:07:33 -0800

    Update avg_spent_per_category.sql

--- sql/financial/avg_spent_per_category.sql
+++ sql/financial/avg_spent_per_category.sql
@@ -1,17 +1,22 @@
--- This code calculates the average amount spent per order per category
+-- This code calculates the average amount spent per order per brand per category
 
 WITH cte AS (
 SELECT
+	p.brand,
 	p.category,
 	oi.order_id,
 	SUM(oi.item_total) AS sum_spent_per_category
     FROM order_items oi
     JOIN products p
 		ON oi.product_id = p.product_id
-GROUP BY oi.order_id,p.category
+	JOIN orders o
+		ON oi.order_id = o.order_id
+	WHERE order_status NOT IN ('returned','cancelled')
+	GROUP BY oi.order_id,p.brand,p.category
 )
 SELECT
-	category,
+	brand,
+	cte.category,
 	ROUND(AVG(sum_spent_per_category),2) AS avg_spent_per_category
     FROM cte
-GROUP BY category
+	GROUP BY brand,category
